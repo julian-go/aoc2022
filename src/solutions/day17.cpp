e07a79f99c3d28174f995bc3d64eb4a590d4ec0a
Julian Gottwald <mail@juliangottwald.com>
2022-12-17 17:48:11 +0100

    day17: part2

--- src/solutions/day17.cpp
+++ src/solutions/day17.cpp
@@ -10,25 +10,50 @@ namespace {
 
 using namespace std;
 
+template <size_t size_x, size_t size_y>
+class RingBuffer {
+ public:
+  RingBuffer()
+  {
+    std::array<bool, size_y> tmp;
+    tmp.fill(false);
+    buffer_.fill(tmp);
+  };
+
+  bool at(size_t x, size_t y) const { return buffer_[x][y % size_y]; }
+
+  void set(size_t x, size_t y, bool value) { buffer_[x][y % size_y] = value; }
+
+  void clearAbove(size_t y_clear, int8_t count)
+  {
+    for (size_t x = 0; x < size_x; ++x) {
+      for (size_t y = y_clear; y < y_clear + count; ++y) {
+        buffer_[x][y % size_y] = false;
+      }
+    }
+  }
+
+  size_t x_max() { return size_x; }
+  size_t y_max() { return size_y; }
+
+ private:
+  array<array<bool, size_y>, size_x> buffer_;
+};
+
 class Tetris {
-  using Block = std::array<std::array<bool, 4>, 4>;
+  using Block = array<array<bool, 4>, 4>;
 
  public:
-  Tetris()
-  {
-    game_state_ = {};
-    for (auto& col : game_state_) {
-      col = {};
-      col.fill(false);
-    }
-
-    spawnBlock();
-  }
+  Tetris() = default;
 
   void process(istream& in)
   {
     string commands;
     getline(in, commands);
+    spawnBlock();
+    int64_t last_floor = 0;
+    int64_t last_count = 0;
+    int64_t floor_bonus = 0;
 
     bool done = false;
     int i = 0;
@@ -43,21 +68,54 @@ class Tetris {
         counter++;
         updateFloor();
         spawnBlock();
-        if (counter == 2022) {
-          cout << floor_;
+        if (counter == 1'000'000'000'000) {
+          cout << "<" << floor_ + floor_bonus << ">" << endl;
           done = true;
+          break;
         }
       }
       ++i;
       i %= commands.size();
-      //printGame();
+      if (i == 0) {
+        done = true;
+        for (size_t x = 0; x < top.x_max(); ++x) {
+          for (size_t y = 0; y < top.y_max(); ++y) {
+            done &= top.at(x, y) == game_state_.at(x, floor_ - y);
+            top.set(x, y, game_state_.at(x, floor_ - y));
+          }
+        }
+      }
+
+      if (i == 0 && !done) {
+        last_floor = floor_;
+        last_count = counter;
+      }
+      if (done) {
+        done = false;
+        /*   cout << "loop:" << endl;
+           cout << "   Floor   " << floor_ - last_floor << endl;
+           cout << "   Counter " << counter - last_count << endl;*/
+
+        // lets feed forward a little
+        int64_t dFloor = floor_ - last_floor;
+        int64_t dCounter = counter - last_count;
+
+        int64_t runs_to_go = (1'000'000'000'000 - counter) / dCounter;
+
+        counter += runs_to_go * dCounter;
+        floor_bonus = runs_to_go * dFloor;
+
+        last_floor = floor_;
+        last_count = counter;
+      }
+      // printGame();
     }
   }
 
   void spawnBlock()
   {
     active_block_++;
-    active_block_ %= 5;
+    active_block_ %= kBlocks.size();
 
     active_block_y_ = floor_ + kSpawnY;
     active_block_x_ = kSpawnX;
@@ -78,10 +136,10 @@ class Tetris {
     return tryMoveBlock(active_block_y_, active_block_x_ + 1);
   }
 
-  bool tryMoveBlock(int32_t new_y, int8_t new_x)
+  bool tryMoveBlock(int64_t new_y, int8_t new_x)
   {
     const Block& block = kBlocks[active_block_];
-    for (int32_t y = 0; y < 4; ++y) {
+    for (int64_t y = 0; y < 4; ++y) {
       for (int8_t x = 0; x < 4; ++x) {
         if (new_y < 0) {
           // floor
@@ -94,7 +152,7 @@ class Tetris {
         if (new_x + x < 0 || new_x + x >= kWidth) {
           continue;
         }
-        if (block[x][y] && game_state_[new_x + x][new_y + y]) {
+        if (block[x][y] && game_state_.at(new_x + x, new_y + y)) {
           return false;
         }
       }
@@ -108,21 +166,28 @@ class Tetris {
   void lockBlock()
   {
     const Block& block = kBlocks[active_block_];
-    for (int32_t y = 0; y < 4; ++y) {
+    for (int64_t y = 0; y < 4; ++y) {
       for (int8_t x = 0; x < 4; ++x) {
         if (block[x][y]) {
-          game_state_[active_block_x_ + x][active_block_y_ + y] = true;
+          game_state_.set(active_block_x_ + x, active_block_y_ + y, true);
         }
+      }
+    }
+
+    for (int64_t y = 0; y < 4; ++y) {
+      bool full = true;
+      for (int8_t x = 0; x < kWidth; ++x) {
+        full &= game_state_.at(x, active_block_y_ + y);
       }
     }
   }
 
   void updateFloor()
   {
-    for (int32_t y = floor_; y < game_state_.front().size(); ++y) {
+    for (int64_t y = floor_; y < floor_ + 10; ++y) {
       bool found_block = false;
       for (int32_t x = 0; x < kWidth; ++x) {
-        if (game_state_[x][y]) {
+        if (game_state_.at(x, y)) {
           found_block = true;
           break;
         }
@@ -132,6 +197,7 @@ class Tetris {
         break;
       }
     }
+    game_state_.clearAbove(floor_ + 4, kSpawnY + 4);
   }
 
   void printBlocks()
@@ -149,12 +215,12 @@ class Tetris {
 
   void printGame()
   {
-    constexpr int32_t context_up = 10;
-    constexpr int32_t context_down = 4000;
-    for (int32_t y = floor_ + context_up; y >= max(floor_ - context_down, 0);
+    constexpr int64_t context_up = 10;
+    constexpr int64_t context_down = 20;
+    for (int64_t y = floor_ + context_up; y >= floor_ - context_down && y >= 0;
          --y) {
       for (int32_t x = 0; x < kWidth; ++x) {
-        string s = (game_state_[x][y] ? "#" : ".");
+        string s = (game_state_.at(x, y) ? "#" : ".");
         if (x >= active_block_x_ && x < active_block_x_ + 4 &&
             y >= active_block_y_ && y < active_block_y_ + 4) {
           const Block& b = kBlocks[active_block_];
@@ -170,38 +236,32 @@ class Tetris {
   }
 
  private:
+  static constexpr int32_t kBufferSize = 30'000;
   static constexpr int8_t kWidth = 7;
   static constexpr int8_t kSpawnX = 2;
   static constexpr int8_t kSpawnY = 3;
 
-  std::array<std::array<bool, 15'000>, kWidth> game_state_;
-  static constexpr std::array<Block, 5> kBlocks = {
-      Block({std::array<bool, 4>({1, 0, 0, 0}),
-             std::array<bool, 4>({1, 0, 0, 0}),
-             std::array<bool, 4>({1, 0, 0, 0}),
-             std::array<bool, 4>({1, 0, 0, 0})}),
-      Block({std::array<bool, 4>({0, 1, 0, 0}),
-             std::array<bool, 4>({1, 1, 1, 0}),
-             std::array<bool, 4>({0, 1, 0, 0}),
-             std::array<bool, 4>({0, 0, 0, 0})}),
-      Block({std::array<bool, 4>({1, 0, 0, 0}),
-             std::array<bool, 4>({1, 0, 0, 0}),
-             std::array<bool, 4>({1, 1, 1, 0}),
-             std::array<bool, 4>({0, 0, 0, 0})}),
-      Block({std::array<bool, 4>({1, 1, 1, 1}),
-             std::array<bool, 4>({0, 0, 0, 0}),
-             std::array<bool, 4>({0, 0, 0, 0}),
-             std::array<bool, 4>({0, 0, 0, 0})}),
-      Block({std::array<bool, 4>({1, 1, 0, 0}),
-             std::array<bool, 4>({1, 1, 0, 0}),
-             std::array<bool, 4>({0, 0, 0, 0}),
-             std::array<bool, 4>({0, 0, 0, 0})})};
+  // std::array<std::array<bool, kBufferSize>, kWidth> game_state_;
+  RingBuffer<kWidth, kBufferSize> game_state_;
+  RingBuffer<kWidth, 10'000> top;
+
+  static constexpr array<Block, 5> kBlocks = {
+      Block({array<bool, 4>({1, 0, 0, 0}), array<bool, 4>({1, 0, 0, 0}),
+             array<bool, 4>({1, 0, 0, 0}), array<bool, 4>({1, 0, 0, 0})}),
+      Block({array<bool, 4>({0, 1, 0, 0}), array<bool, 4>({1, 1, 1, 0}),
+             array<bool, 4>({0, 1, 0, 0}), array<bool, 4>({0, 0, 0, 0})}),
+      Block({array<bool, 4>({1, 0, 0, 0}), array<bool, 4>({1, 0, 0, 0}),
+             array<bool, 4>({1, 1, 1, 0}), array<bool, 4>({0, 0, 0, 0})}),
+      Block({array<bool, 4>({1, 1, 1, 1}), array<bool, 4>({0, 0, 0, 0}),
+             array<bool, 4>({0, 0, 0, 0}), array<bool, 4>({0, 0, 0, 0})}),
+      Block({array<bool, 4>({1, 1, 0, 0}), array<bool, 4>({1, 1, 0, 0}),
+             array<bool, 4>({0, 0, 0, 0}), array<bool, 4>({0, 0, 0, 0})})};
 
   int8_t active_block_ = -1;
   int8_t active_block_x_ = 0;
-  int32_t active_block_y_ = 0;
-  int32_t floor_ = 0;
-  int32_t counter = 0;
+  int64_t active_block_y_ = 0;
+  int64_t floor_ = 0;
+  int64_t counter = 0;
 };
 
 inline Solution part1(std::ifstream& in)
