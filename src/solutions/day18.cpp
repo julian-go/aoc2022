c1691d01639f9516ee8a694cc0571b5a668c2ed3
Julian Gottwald <mail@juliangottwald.com>
2022-12-19 15:24:49 +0100

    day18: remove commented code

--- src/solutions/day18.cpp
+++ src/solutions/day18.cpp
@@ -60,100 +60,6 @@ inline Solution part1(std::ifstream& in)
   return std::to_string(exposed_sides);
 }
 
-// bool checkPocket(set<Vector3D>& droplets, int32_t& exposed_sides,
-//                  const Vector3D& v, int16_t depth)
-//{
-//   constexpr int16_t kMaxDepth = 60;
-//   if (depth > kMaxDepth) {
-//     return false;
-//   }
-//
-//   bool is_pocket = true;
-//   droplets.insert(v);
-//   exposed_sides += 6;
-//   for (const auto& n : kNeighbors) {
-//     if (!droplets.contains(v + n)) {
-//       droplets.insert(v + n);
-//       is_pocket &= checkPocket(droplets, exposed_sides, v + n, depth + 1);
-//     } else {
-//       exposed_sides -= 2;
-//     }
-//   }
-//
-//   return is_pocket;
-// }
-
-// bool fillVolume(vector<vector<vector<bool>>>& volume,
-//                 const Vector3D& volume_size, int32_t& exposed_sides,
-//                 Vector3D v)
-//{
-//   bool is_pocket = true;
-//   if (v.x >= volume_size.x || v.x < 0 || v.y >= volume_size.y || v.y < 0 ||
-//       v.z >= volume_size.z || v.z < 0) {
-//     return false;
-//   }
-//
-//   //volume[1][1][1] = true;
-//   //cout << v.x << endl;
-//   //volume[v.x][1][1] = true;
-//   //volume[v.x][v.y][1] = true;
-//   volume[v.x][v.y][v.z] = true;
-//   cout << "filled at " << v.x << " " << v.y << " " << v.z << endl;
-//   exposed_sides += 6;
-//   for (const auto& n : kNeighbors) {
-//     const auto vn = v + n;
-//     if (vn.x >= volume_size.x || vn.x < 0 || vn.y >= volume_size.y ||
-//         vn.y < 0 || vn.z >= volume_size.z || vn.z < 0) {
-//       is_pocket = false;
-//       continue;
-//     }
-//     if (!volume[vn.x][vn.y][vn.z]) {
-//       is_pocket &= fillVolume(volume, volume_size, exposed_sides, vn);
-//     } else {
-//       exposed_sides -= 2;
-//     }
-//   }
-//
-//   return is_pocket;
-// }
-
-// inline Solution part2(std::ifstream& in)
-//{
-//   Vector3D v;
-//   Vector3D volume_size;
-//   int32_t exposed_sides = 0;
-//   char sep;
-//   set<Vector3D> droplets;
-//   while (in >> v.x >> sep >> v.y >> sep >> v.z) {
-//     if (droplets.contains(v)) {
-//       continue;
-//     }
-//
-//     // first insert new droplet
-//     droplets.insert(v);
-//     exposed_sides += 6;
-//     for (const auto& n : kNeighbors) {
-//       if (droplets.contains(v + n)) {
-//         exposed_sides -= 2;
-//       }
-//     }
-//
-//     // Then check all neighbors for an airpocket
-//
-//     for (const auto& n : kNeighbors) {
-//       if (!droplets.contains(v + n)) {
-//         set<Vector3D> tmp_droplets = droplets;
-//         int32_t tmp_sides = exposed_sides;
-//         if (checkPocket(tmp_droplets, tmp_sides, v + n, 0)) {
-//           droplets = tmp_droplets;
-//           exposed_sides = tmp_sides;
-//         }
-//       }
-//     }
-//   }
-//   return std::to_string(exposed_sides);
-// }
-
 inline Solution part2(std::ifstream& in)
 {
   Vector3D v;
@@ -182,27 +88,6 @@ inline Solution part2(std::ifstream& in)
     vector<vector<bool>> tmp_y(volume_size.y + 2, tmp_z);
     volume.resize(volume_size.x + 2, tmp_y);
   }
-  // for (const auto& d : droplets) {
-  //   volume[d.x][d.y][d.z] = true;
-  // }
-
-  // for (int x = 0; x < volume_size.x; ++x) {
-  //   for (int y = 0; y < volume_size.y; ++y) {
-  //     for (int z = 0; z < volume_size.z; ++z) {
-  //       if (!volume[x][y][z]) {
-  //         auto filled_volume = volume;
-  //         auto filled_exposed_sides = exposed_sides;
-  //         if (fillVolume(filled_volume, volume_size, filled_exposed_sides,
-  //                        Vector3D(x, y, z))) {
-  //           cout << "POCKET " << x << " " << y << " " << z << " " << endl;
-  //           cout << "Sides " << filled_exposed_sides << " " << endl;
-  //           volume = filled_volume;
-  //           exposed_sides = filled_exposed_sides;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
 
   stack<Vector3D> to_check;
   to_check.push(Vector3D(0, 0, 0));
